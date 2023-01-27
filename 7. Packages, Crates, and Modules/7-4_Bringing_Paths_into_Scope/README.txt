056f75da9e1818eb7560b94b723e5340cc93790c
Ryan Meneses <rmeneses@rigetti.com>
2023-01-26 22:04:43 -0800

    Add the use keyword.

--- 7. Packages, Crates, and Modules/7-4_Bringing_Paths_into_Scope/README.txt
+++ 7. Packages, Crates, and Modules/7-4_Bringing_Paths_into_Scope/README.txt
@@ -1,1 +1,7 @@
 Bringing Paths into Scope with the `use` Keyword
+Use `use` to bring a function into scope to prevent having to specifying the absolute or relative paths everytime.
+
+`use` is similar to creating a symbolic link in a filesystem. `use crate::parent_module_name::child_module_name` brings
+its functions into scope. 
+
+Ensure that `use` is used in its scope, use super::module_name or add the use statement into the separate module.
--- 7. Packages, Crates, and Modules/7-4_Bringing_Paths_into_Scope/restaurant/src/lib.rs
+++ 7. Packages, Crates, and Modules/7-4_Bringing_Paths_into_Scope/restaurant/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use crate::front_of_house::hosting;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+// Use makes it so that a modules functions can be used in other parts of the code.
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+// Use `super` to access the another modules modules functions outside of the scope of this module or
+// add the use statement into the module itself.
+mod customer {
+    pub fn eat_at_restaurant() {
+        super::hosting::add_to_waitlist();
     }
 }
